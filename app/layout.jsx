fb79185707196c6b33a004bf35493ce70bd15c90
xiepeng <xiepeng1992@gmail.com>
2024-03-18 23:45:49 +0800

    Add properties

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import '@/assets/styles/globals.css';
 import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
 
 export const metadata = {
   title: 'PropertyPulse | Find The Perfect Rental',
@@ -13,6 +14,7 @@ const MainLayout = ({ children }) => {
       <body>
         <Navbar />
         <main>{children}</main>
+        <Footer />
       </body>
     </html>
   )
--- app/properies/page.jsx
+++ app/properies/page.jsx
@@ -1,11 +1,18 @@
-import Link from "next/link";
+import properties from '@/properties.json';
 
 const ProperiesPage = () => {
   return (
-    <div>
-      <h1 className="text-3xl">Properties</h1>
-      <Link href='/properties'>Show Properties</Link>
-    </div>
+    <section class="px-4 py-6">
+      <div class="container-xl lg:container m-auto px-4 py-6">
+        { properties.length === 0 ? ( <p> No properties found</p>) : (
+          <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {properties.map((property) => {
+              <div> {property.name} </div>
+            })}
+          </div>
+        )}
+      </div>
+    </section>
   )
 }
 
